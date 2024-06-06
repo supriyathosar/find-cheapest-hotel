76b6ec722252baa49bcbb55f6858e07aa485cfc6
Supriya Thosar <10380458+supriyathosar@users.noreply.github.com>
2024-06-06 20:01:36 +0530

    Update ReservationSystem.java

--- src/main/ReservationSystem.java
+++ src/main/ReservationSystem.java
@@ -13,4 +13,18 @@ public class ReservationSystem {
     public Hotel findCheapestHotel() {
         return hotels.stream().sorted(Comparator.comparingDouble(Hotel::getRate)).findFirst().get();
     }
+
+    public Hotel findCheapestHotel(String day){
+
+        if(Object.nonNull(day){
+                    return hotels.stream.filter(Objects :: nonNull).filter(hotel -> Objects.nonNull(hotel)
+                                         .filter(hotel -> Objects.nonNull(hotel.getRateMap.get(day)))
+                                        .min(Compartor.comparingDouble(hotel -> hotel.getRateMap.get(day))).orElse(null);                                        
+        }else{
+                     return hotels.stream.filter(Objects :: nonNull).filter(hotel -> Objects.nonNull(hotel)
+                                         .filter(hotel -> Objects.nonNull(hotel.getRateMap.get(Instant.now().truncatedTo(ChornoUnit.DAYS).toString())))
+                                        .min(Compartor.comparingDouble(hotel -> hotel.getRateMap.get(Instant.now().truncatedTo(ChronoUnit.DAYS).toString()))).orElse(null);                                        
+        }
+        
+    }
 }
