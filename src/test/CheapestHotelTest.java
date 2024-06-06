2a797be8d1970d911c5af9f929d60c5d69f05fa8
Supriya Thosar <10380458+supriyathosar@users.noreply.github.com>
2024-06-06 20:08:03 +0530

    Update CheapestHotelTest.java

--- src/test/CheapestHotelTest.java
+++ src/test/CheapestHotelTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit.
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,4 +27,26 @@ public class CheapestHotelTest {
         Hotel cheapestHotel = reservationSystem.findCheapestHotel();
         assertEquals(MiamiBeach, cheapestHotel);
     }
+
+        @Test
+    public void findCheapestHotel(){
+        Map rateMapMiamiBeach = new HashMap<String,Double>();
+        rateMapMiamiBeach.put(Instant.now().truncatedTo(ChornoUnit.DAYS).toString(),80D);
+         Map rateMapMiamiDowntown = new HashMap<String,Double>();
+        rateMapMiamiDowntown.put(Instant.now().truncatedTo(ChornoUnit.DAYS).toString(),120D);
+         Map rateMapMiamiMidtown = new HashMap<String,Double>();
+        rateMapMiamiMidtown.put(Instant.now().truncatedTo(ChornoUnit.DAYS).toString(),100D);
+        
+        Hotel MiamiBeach = new Hotel(rateMapMiamiBeach, "Miami Beach");
+        Hotel MiamiDowntown = new Hotel(rateMapMiamiDowntown, "Miami Downtown");
+        Hotel MiamiMidtown = new Hotel(rateMapMiamiMidtown, "Miami Midtown");
+        List<Hotel> hotelList = new ArrayList<>();
+        hotelList.add(MiamiDowntown);
+        hotelList.add(MiamiBeach);
+        hotelList.add(MiamiMidtown);
+        ReservationSystem reservationSystem = new ReservationSystem(hotelList);
+        Hotel cheapestHotel = reservationSystem.findCheapestHotel(Instant.now().truncatedto(ChronoUnit.DAYS).toString());
+        assertEquals(MiamiBeach, cheapestHotel);
+    }
+    
 }
